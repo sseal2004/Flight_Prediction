d043bc4c44a4a941d5077e7c20bac3403ec3ae64
Soumyadipta Seal <s.seal.a.b.c@gmail.com>
2026-02-17 22:28:16 +0530

    all changes

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -27,7 +27,7 @@ function FlightPricePredictor() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://127.0.0.1:5000/predict', formData);
+      const response = await axios.post('https://flight-prediction-buuw.onrender.com/predict', formData);
       setPrediction(response.data.prediction);
     } catch (error) {
       console.error('Error fetching prediction:', error);
